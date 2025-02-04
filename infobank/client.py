c2292196bffd5f4aac318bda82b313f3cc31be99
jino <jino@infobank.net>
2025-02-04 14:09:33 +0900

    refactor: 정보 수정

--- infobank/client.py
+++ infobank/client.py
@@ -1,8 +1,4 @@
 class Client:
-    api_url= "http://172.16.0.73:7000"
-    client_id = "tb_EMMA_jino"
-    client_passwd = "1234"
-    
-    # api_url = "https://omni.ibapi.kr"
-    # client_id = "gw_omni_real01"
-    # client_passwd = "12345678"
+    api_url= "<api_url>"
+    client_id = "<client_id>"
+    client_passwd = "<client_passwd>"
